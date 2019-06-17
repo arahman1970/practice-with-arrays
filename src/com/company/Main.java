787c21cb2458c5668deb0f29fc911ba0545bb8f2
arahman1976 <ar.nj.usa@gmail.com>
2019-06-17 17:11:21 -0400

    practice with git

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // array to hold numbers 1-10
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = sum + numbers[i];
+
+            // write your code here
+        }
+        System.out.println("The sum is : " +sum);
+    }}
+
+    // get the sum of 1-10
+
+\\ below : Create an array to hold 10 number inputs from the user and print that total to the screen.
+
+
+
+
