903d9210bf85367cc7bf8df2d26270f60b02f19f
Abdul Rahman <ar.nj.usa@gmail.com>
2019-06-19 09:01:29 -0400

    june 19

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -81,34 +81,33 @@ public class Main {
 //            }
 //        }
 // #7 Declare an array with the numbers 1-10. Print the numbers 1-10 backwards from the array.
-//int [] num10 = {1,2,3,4,5,6,7,8,9,10};
-//        for (int n =9; n>-1; n--){
-//            System.out.println(" In reverse " +num10[n]);
-//        }
+int [] num10 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("#7");
+        for (int n =9; n>-1; n--){
+            System.out.println(" In reverse " +num10[n]);
+        }
 
-//        Given the following array: ['w','t','y','h','k']
+//
+//        #8 Given the following array: ['w','t','y','h','k']
 //        Loop through the array and replace the the letter 't' with the word "hello" once it's found.
 
-//        char [] origchar = {'w','t','y','h','k'};
-//        String nowstring = new String(origchar);
-//
-//        //System.out.println(nowstring);
-//
-//        String [] newstr = new String[10];
-//        newstr[0] = nowstring;
-//
-//        //System.out.println(nowstring);
-//
-//        //String nowstring = new String()
-//        for (int o=0; o <1; o++);
-//            if (newstr[0]="t"){
-//                newstr[0]="hello";
-//            }
-//
+        char [] origchar = {'w','t','y','h','k'};
+        String nowstring = new String(origchar);
 
-    }
+        //System.out.println(nowstring);
 
-}
+        String [] newstr = new String[10];
+        newstr[0] = nowstring;
+
+        //System.out.println(nowstring);
+        System.out.println("#8");
+
+        //String nowstring = new String()
+        for (int o=0; o <1; o++);
+            if (newstr[0]="t"){
+                newstr[0]="hello";
+            }
+
 
 
 //9. Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
@@ -116,4 +115,17 @@ public class Main {
 //Expected output:
 //(7,7) and (6,6)
 
+        int arr1[] = {1, 7, 6, 5, 9};
+        int arr2[] = {2, 7, 6, 3, 4};
+        System.out.println("#9");
+        for (int p = 0; p < arr1.length; p++) {
+            for (int q = 0; q < arr2.length; q++) {
+                if (arr1[p] == arr2[q]) {
+                    System.out.println(arr1[p] + " is a matching pair");
+                }
+                // }
 
+            }
+        }
+    }
+}
